ddc6e0061b89727ebc8965d95851810e72903e62
gracianow <gracianow@msn.com>
2024-01-22 21:25:48 -0300

    codebot without openAI

--- website/views.py
+++ website/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+	
+	lang_list=['c', 'clike', 'csharp', 'cshtml', 'css', 'css-extras', 'csv', 'dart', 'django', 'go', 'html', 'java', 'javascript', 'js-templates', 'markdown', 'markup', 'markup-templating', 'mongodb', 'perl', 'php', 'plsql', 'powerquery', 'powershell', 'python', 'r', 'ruby', 'rust', 'sql', 'swift', 'typescript', 'typoscript', 'yaml']
+
+	if request.method == "POST":
+		code = request.POST['code']
+		lang = request.POST['lang']
+		return render(request, 'home.html', {'lang_list':lang_list, 'code':code, 'lang':lang})
+
+		if lang == "Select Programming Languages":
+			messages.success(request, "Hey you forgot to PICK A Programming Languages...")
+			return render(request, 'home.html', {'lang_list':lang_list, 'code':code, 'lang':lang})
+
+	return render(request, 'home.html', {'lang_list':lang_list})
